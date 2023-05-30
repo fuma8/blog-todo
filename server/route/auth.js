e1e9814da754df3e3cd0198e8ed0a156de66dcae
fuma8 <fuma.kimishima.3c@stu.hosei.ac.jp>
2023-05-30 17:09:01 +0900

    first commit

--- server/route/auth.js
+++ server/route/auth.js
@@ -2,7 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 router.post("/register", (req, res) => {
-    
+
 })
+
+router.post("/login", (req, res) => {
+
+})
+
+router.post("/logout", (req, res) => {
+
+})
+
 
 module.exports = router
